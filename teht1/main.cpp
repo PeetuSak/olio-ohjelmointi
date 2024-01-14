cbcce3bd5eb6f35f7d6c5b5aa4a75be14149f43b
{Peetu <{peetu.sakkinen@gmail.com}>
2024-01-14 15:58:47 +0200

    teht1 valmis

---
+++ teht1/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+int numero;
+
+int main()
+{
+    srand(time(NULL));
+    int satunnaisluku = rand() % 20;
+
+    do{
+        cout << "Anna luku" << endl;
+        cin >> numero;
+
+        if (numero > satunnaisluku){
+            cout << "satunnaisluku on pienempi"<< endl;
+        }
+        else if (numero < satunnaisluku){
+            cout << "satunnaisluku on suurempi"<< endl;
+        }
+        else{
+            cout << "numero on oikein"<< endl;
+        }
+    } while (numero != satunnaisluku);
+
+    return 0;
+}
